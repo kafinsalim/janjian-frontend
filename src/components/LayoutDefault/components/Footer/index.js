e2154915d7e7e0ae980d1cd7a173be427835f37e
Kafin Salim <kafinsalim@gmail.com>
2019-06-05 22:47:27 +0700

    add _redirect

--- src/components/LayoutDefault/components/Footer/index.js
+++ src/components/LayoutDefault/components/Footer/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import './style.css'
 
 export default class Footer extends Component {
   render() {
     return <div className="panel-footer">
-      Start a new group | Log out<br/><br/>
-      © 2019 Janjian | Janjian is a react project for learning by kafin salim.
+      <Link to='/create'  style={{color: "#FF880A"}}>Create New Janji</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/logout'>Log Out</Link>
+      <br/><br/>
+      © 2019 Janjian | Janjian is a react project for learning purpose by kafin salim.
       <br/><br/>
     </div>;
   }
